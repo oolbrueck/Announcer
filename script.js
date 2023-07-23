8dbbb21bae7c30c95204aa3cf448500f6243cc5c
Oliver Olbrück <oligo99@t-online.de>
2023-07-23 17:07:17 +0200

    adds number of repetitions and how long the break between the words shall last

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
+var words = [];
+
 function convertTextToSpeech() {
-  var text = document.getElementById('text').value;
-  var words = text.split(' ');
-  
-  playWords(words, 0);
+  console.log("words: " + words);
+  let playlist = createPlaylist();
+  console.log(playlist);
+  playWords(playlist, 0);
 }
 
 function playWords(words, index) {
+  const breakInMilliseconds = document.getElementById('break-time').value;
+
   if (index < words.length) {
     var utterance = new SpeechSynthesisUtterance(words[index]);
     
     utterance.onend = function() {
       setTimeout(function() {
         playWords(words, index + 1);
-      }, 200); // Pausendauer zwischen den Wörtern in Millisekunden (hier: 200ms)
+      }, breakInMilliseconds); // Pausendauer zwischen den Wörtern in Millisekunden (hier: 200ms)
     };
     
     speechSynthesis.speak(utterance);
   }
 }
+
+function addChip() {
+  const chipsContainer = document.getElementById('chips-container');
+  const chipInput = document.getElementById('chip-input');
+
+  const chipText = chipInput.value;
+
+  if (chipText.trim() !== '') {
+    const chipDiv = document.createElement('div');
+    chipDiv.classList.add('chip');
+
+    words.push(chipText);
+    chipDiv.textContent = chipText;
+
+    const closeBtn = document.createElement('span');
+    closeBtn.classList.add('closebtn');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.onclick = function() {
+      this.parentElement.style.display = 'none';
+      console.log(this.parentElement.childNodes[0].textContent);
+      const index = words.indexOf(this.parentElement.childNodes[0].textContent);
+      if (index !== -1) {
+        words.splice(index, 1);
+      }
+    };
+    chipDiv.appendChild(closeBtn);
+    chipsContainer.appendChild(chipDiv);
+    chipInput.value = '';
+  }
+}
+
+function createPlaylist() {
+  let playlist = [];
+  const numberOfAnnouncements = document.getElementById('number-of-announcements-input').value;
+  console.log("numberOfAnnouncements" + numberOfAnnouncements)
+
+  for(let i = 0; i < numberOfAnnouncements; i++) {
+    playlist.push(words[Math.floor(Math.random() * words.length)])
+  }
+  return playlist;
+}
+
